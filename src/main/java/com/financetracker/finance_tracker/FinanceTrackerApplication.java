63eed6047f4b7e8ec538b9abe9c714431eea636e
Singh <janhvi.singh@hpe.com>
2026-01-07 15:17:22 +0530

    Added UI using html and javascript

--- src/main/java/com/financetracker/finance_tracker/FinanceTrackerApplication.java
+++ src/main/java/com/financetracker/finance_tracker/FinanceTrackerApplication.java
@@ -2,12 +2,28 @@ package com.financetracker.finance_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FinanceTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinanceTrackerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+					.allowedOrigins("*")
+					.allowedMethods("GET", "POST", "PUT", "DELETE")
+					.allowedHeaders("*");
+			}
+		};
 	}
 
 }
